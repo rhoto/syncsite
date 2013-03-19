89876ef40ef8f709fb1baabeda77d2aae979e4eb
rhoto <rhoto@gmail.fake>
2013-03-19 17:16:00 -0500

    very basic websocket

---
+++ server.py
@@ -0,0 +1,24 @@
+import sys
+import json
+
+from twisted.internet import reactor
+from twisted.python import log
+
+from autobahn.websocket import WebSocketServerFactory, WebSocketServerProtocol, listenWS
+
+class SyncServerProtocol(WebSocketServerProtocol):
+
+	def onMessage(self, msg, binary):
+		msg = json.dumps(msg.split(' '))
+		print "sending echo:", msg
+		self.sendMessage(msg, False)
+
+if __name__ == '__main__':
+
+	log.startLogging(sys.stdout)
+
+	factory = WebSocketServerFactory("ws://localhost:9000", debug = False)
+	factory.protocol = SyncServerProtocol
+	listenWS(factory)
+
+	reactor.run()
